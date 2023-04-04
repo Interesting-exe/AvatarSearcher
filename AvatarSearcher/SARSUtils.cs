9f83be7eaa7018f6a0f28fcb39e64506053ae487
Interesting <ozshwartz057@gmail.com>
2023-04-04 18:07:06 +0300

    updated api i think

--- AvatarSearcher/SARSUtils.cs
+++ AvatarSearcher/SARSUtils.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using MelonLoader;
 using Newtonsoft.Json;
@@ -12,104 +13,60 @@ namespace AvatarSearcher
 {
     public class SARSUtils
     {
-        private static string baseUrl = "https://unlocked.ares-mod.com/records/";
-
-        private static string apiUrl =
-            $"{baseUrl}Avatars?include=Created,TimeDetected,AvatarID,AvatarName,AvatarDescription,AuthorID,AuthorName,PCAssetURL,QUESTAssetURL,ImageURL,ThumbnailURL,UnityVersion,Releasestatus,Tags,Pin,PinCode&order=Created,desc&";
-
-        private static string apiKey = null;
-
-        public static async void OnIntialize()
-        {
-            if (File.Exists($"{Environment.CurrentDirectory}\\AvatarSearch\\APIKey.cum"))
-            {
-                apiKey = File.ReadAllText($"{Environment.CurrentDirectory}\\AvatarSearch\\APIKey.cum");
-            }
-
-            if (apiKey == null)
-            {
-                MelonLogger.Msg("Enter SARS API key:");
-                apiKey = Console.ReadLine();
-            }
-
-            HttpClient httpClient = new HttpClient();
-            
-            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("SARS V1.1.2.10");
-
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"{apiUrl}&size=1");
-            
-            request.Headers.Add("X-API-Key", apiKey);
-            
-            HttpResponseMessage response = await httpClient.SendAsync(request);
-
-            if (response.IsSuccessStatusCode)
-            {
-                MelonLogger.Msg("API Key Valid!");
-                if(!Directory.Exists($"{Environment.CurrentDirectory}\\AvatarSearch"))
-                    Directory.CreateDirectory($"{Environment.CurrentDirectory}\\AvatarSearch");
-                File.WriteAllText($"{Environment.CurrentDirectory}\\AvatarSearch\\APIKey.cum", apiKey);
-            }
-            else
-            {
-                if(response.StatusCode == HttpStatusCode.Forbidden)
-                {
-                    MelonLogger.Error("Invalid Key!");
-                    MelonLogger.Msg("Enter new key:");
-                    apiKey = Console.ReadLine();
-                    if(!Directory.Exists($"{Environment.CurrentDirectory}\\AvatarSearch"))
-                        Directory.CreateDirectory($"{Environment.CurrentDirectory}\\AvatarSearch");
-                    File.WriteAllText($"{Environment.CurrentDirectory}\\AvatarSearch\\APIKey.cum", apiKey);
-                    OnIntialize();
-                }
-            }
-        }
+        private static string baseUrl = "https://unlocked.shrektech.xyz/Avatar/GetPublicAvatar";
 
         public static async Task<List<Avatar>> Search(string search)
         {
-            string url = apiUrl + $"filter=AvatarName,cs,{search}";
-            url += $"&filter=Releasestatus,eq,public";
-            url += $"&filter=PCAssetURL,sw,https://";
-            url += $"&size={1000}";
-
             HttpClient httpClient = new HttpClient();
 
-            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("SARS V1.1.2.10");
+            var jsonObj = new{
+                avatarName = search,
+                amount = 10
+            };
 
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+            var content = new StringContent(JsonConvert.SerializeObject(jsonObj), Encoding.UTF8, "application/json");
 
-            request.Headers.Add("X-API-Key", apiKey);
-
-            HttpResponseMessage response = await httpClient.SendAsync(request);
+            HttpResponseMessage response = await httpClient.PostAsync(baseUrl, content);
 
             var jsonResponse = await response.Content.ReadAsStringAsync();
-            
-            AvatarList avatarList = JsonConvert.DeserializeObject<AvatarList>(jsonResponse);
-            
-            return avatarList.records;
+
+            var avi = JsonConvert.DeserializeObject<List<Root>>(jsonResponse);
+
+            List<Avatar> avatars = new List<Avatar>();
+
+            foreach (var item in avi)
+            {
+                avatars.Add(item.avatar);
+            }
+
+            return avatars;
         }
         
         public static async Task<List<Avatar>> SearchAuthor(string search)
         {
-            string url = apiUrl + $"filter=AuthorID,eq,{search}";
-            url += $"&filter=Releasestatus,eq,public";
-            url += $"&filter=PCAssetURL,sw,https://";
-            url += $"&size={1000}";
-
             HttpClient httpClient = new HttpClient();
+            
+            var jsonObj = new{
+                authorId = search,
+                amount = 10
+            };
 
-            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("SARS V1.1.2.10");
+            var content = new StringContent(JsonConvert.SerializeObject(jsonObj), Encoding.UTF8, "application/json");
 
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
-
-            request.Headers.Add("X-API-Key", apiKey);
-
-            HttpResponseMessage response = await httpClient.SendAsync(request);
+            HttpResponseMessage response = await httpClient.PostAsync(baseUrl, content);
 
             var jsonResponse = await response.Content.ReadAsStringAsync();
-            
-            AvatarList avatarList = JsonConvert.DeserializeObject<AvatarList>(jsonResponse);
-            
-            return avatarList.records;
+
+            var avi = JsonConvert.DeserializeObject<List<Root>>(jsonResponse);
+
+            List<Avatar> avatars = new List<Avatar>();
+
+            foreach (var item in avi)
+            {
+                avatars.Add(item.avatar);
+            }
+
+            return avatars;
         }
     }
 }
